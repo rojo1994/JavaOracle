1301e4a5ffdf1e61191ab4f95383339a358d1f25
Hector Hernandez Angulo <38303449+rojo1994@users.noreply.github.com>
2024-06-10 10:37:40 -0700

    Delete Desktop/Programacion/WebDevelpmentProjects directory

--- Desktop/Programacion/WebDevelpmentProjects/BackEnd/2.3 NPM/index.js
+++
@@ -1,9 +0,0 @@
-import generateName from "sillyname";
-import superheroes from "superheroes"
-var sillyname = generateName();
-
-console.log(`My name is ${sillyname}`);
-
-
-console.log("I am " + superheroes.random());
-
